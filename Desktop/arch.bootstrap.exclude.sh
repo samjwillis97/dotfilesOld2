6a5bd0298c021cdfeb4335ab4b39c57ed090aaad
Sam Willis <samjwillis97@gmail.com>
2021-07-21 18:33:12 +1000

    changed file name added some more comments

--- Desktop/arch.bootstrap.exclude.sh
+++
@@ -1,4 +0,0 @@
-# Symlink all dotfiles
-## Ignore errors because files may already exist
-
-# 
---
+++ Desktop/bootstrap.exclude.sh
@@ -0,0 +1,9 @@
+# Symlink all dotfiles
+## Ignore errors because files may already exist
+
+# Install Requirement
+## Change per OS
+
+# Create Some Folders as required
+
+# Create crontabs as required
